0329b7c87aa3bdc1592dd75ca3a413856293c639
mateus-oliveira <matews5522@gmail.com>
2020-05-09 19:35:25 -0300

    Create Tarefa in models.py/ Create forms.py/ Create base.html/ Configure connection to MySQL/ Create tarefa on database

--- gerenciador_tarefas/app/urls.py
+++ gerenciador_tarefas/app/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path(
-        'listar_tarefas/',
-        listar_tarefas,
-        name='listar_tarefas',
-    ),
+    path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
+    path('cadastrar_tarefas/', cadastrar_tarefa, name='cadastrar_tarefa'),
 ]
--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import TarefaForm
+from .entidades.tarefa import Tarefa
+from .services import tarefa_service
 
 # Create your views here.
 
 def listar_tarefas(request):
     nome_tarefa = "Estudar para o ENEM"
     return render(request, 'tarefas/listar_tarefas.html', {"nome": nome_tarefa})
+
+def cadastrar_tarefa(request):
+    if request.method == 'POST': 
+        form_tarefa = TarefaForm(request.POST)
+        if form_tarefa.is_valid():
+            titulo = form_tarefa.cleaned_data["titulo"]
+            descricao = form_tarefa.cleaned_data["descricao"]
+            data_expiracao = form_tarefa.cleaned_data["data_expiracao"]
+            prioridade = form_tarefa.cleaned_data["prioridade"]
+            tarefa_nova = Tarefa(
+                titulo=titulo,
+                descricao=descricao,
+                data_expiracao=data_expiracao,
+                prioridade=prioridade,
+            )
+            tarefa_service.cadastrar_tarefa(tarefa_nova)
+            return redirect('listar_tarefas')
+    else:
+        form_tarefa = TarefaForm()
+    
+    return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
